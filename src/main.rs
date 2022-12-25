6bf8e42ffb6a50be02d3fd0b5e3b7f3366210fb6
devkcud <andrescalisejr@gmail.com>
2022-12-24 23:48:05 -0300

    feat(todo): Add file/folder creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{ fs::{ File, create_dir_all, read_to_string }, path::Path };
+
+use dirs::config_dir;
+
 fn main() {
-    println!("Hello, world!");
+    let todos = load_todo_list();
+    println!("{}", todos);
+}
+
+fn load_todo_list() -> String {
+    let folder_path: String = format!("{}/stodo", config_dir().unwrap().to_str().unwrap());
+    let file_path: String = format!("{}/todos.txt", &folder_path);
+
+    create_dir_all(&folder_path).expect("Could not create todo folder.");
+
+    if !Path::new(&file_path).is_file() {
+        File::create(&file_path).expect("Could not create todo file.");
+    }
+
+    return read_to_string(&file_path).expect("Could not read todo file.").trim().to_string();
 }
