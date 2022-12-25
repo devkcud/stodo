2bff1f9d2b853270f6071e016c048dd7757f7c33
devkcud <andrescalisejr@gmail.com>
2022-12-25 03:03:33 -0300

    chore(todo): Add function get todo by id

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::{ fs::{ File, create_dir_all, read_to_string }, path::Path };
 use dirs::config_dir;
 
 fn main() {
-    let todos = load_todo_list();
-    println!("{}", todos);
+    let todos: String = get_todos();
+    println!("{}", get_todo_by_id(&todos, 3).unwrap_or(String::new()));
 }
 
-fn load_todo_list() -> String {
+fn get_todos() -> String {
     let folder_path: String = format!("{}/stodo", config_dir().unwrap().to_str().unwrap());
     let file_path: String = format!("{}/todos.txt", &folder_path);
 
@@ -18,4 +18,16 @@ fn load_todo_list() -> String {
     }
 
     return read_to_string(&file_path).expect("Could not read todo file.").trim().to_string();
+}
+
+fn get_todo_by_id(todos: &str, id: usize) -> Result<String, String> {
+    if todos.len() == 0 {
+        return Err(String::from("Todo list is empty."));
+    }
+
+    if todos.lines().nth(id).is_none() {
+        return Err(format!("Item at index {} is null", id));
+    }
+
+    return Ok(String::from(todos.lines().nth(id).unwrap_or("")));
 }
