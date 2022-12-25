d01d2465b089e4166155fe48b85981c3ddeb8563
devkcud <andrescalisejr@gmail.com>
2022-12-25 05:58:14 -0300

    feat(todo): Add basic todo management

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod utils;
 use utils::{ config::init_config, todoman::get_todo_list };
 
-use crate::utils::todoman::get_todo_by_id;
+use crate::utils::todoman::{ get_todo_by_id, clear_todo_list, add_todo, remove_todo };
 
 fn main() {
     let config = init_config();
 
-    println!("{}", get_todo_list(&config));
+    clear_todo_list(&config);
+    add_todo(&config, String::from("Hello world! 0"));
+    add_todo(&config, String::from("Hello world! 1"));
+    add_todo(&config, String::from("Hello world! 2"));
+    add_todo(&config, String::from("Hello world! 3"));
+    add_todo(&config, String::from("Hello world! 4"));
+    add_todo(&config, String::from("Hello world! 5"));
+    add_todo(&config, String::from("Hello world! 6"));
+    add_todo(&config, String::from("Hello world! 7"));
+    add_todo(&config, String::from("Hello world! 8"));
+    remove_todo(&config, 1);
+    println!("{}\n", get_todo_list(&config));
     println!("{}", get_todo_by_id(&config, 0).expect(""));
 }
--- src/utils/todoman.rs
+++ src/utils/todoman.rs
@@ -1,4 +1,6 @@
-use std::fs::read_to_string;
+use std::fs::{ read_to_string, write, OpenOptions };
+use std::io::prelude::*;
+use std::ops::Sub;
 
 use super::config::Configuration;
 
@@ -21,4 +23,35 @@ pub fn get_todo_by_id(config: &Configuration, id: usize) -> Result<String, Strin
     }
 
     return Ok(String::from(todo_list.lines().nth(id).unwrap()));
+}
+
+pub fn add_todo(config: &Configuration, todo: String) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&config.todo_list_file)
+        .unwrap();
+
+    writeln!(file, "{}", todo).expect(
+        format!("Could not write file: {}", &config.todo_list_file).as_str()
+    );
+}
+
+pub fn remove_todo(config: &Configuration, id: usize) {
+    let todo_list = get_todo_list(&config);
+
+    if id > todo_list.lines().count().sub(1) {
+        return ();
+    }
+
+    let mut lines = todo_list.lines().collect::<Vec<&str>>();
+    lines.remove(id);
+
+    write(&config.todo_list_file, lines.join("\n")).unwrap();
+}
+
+pub fn clear_todo_list(config: &Configuration) {
+    write(&config.todo_list_file, "").expect(
+        format!("Could not write file: {}", &config.todo_list_file).as_str()
+    );
 }
