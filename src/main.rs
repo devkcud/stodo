69988335005dcb5fe531f8e34548656dc2eaffb7
devkcud <andrescalisejr@gmail.com>
2022-12-25 05:07:24 -0300

    feat(config): Add configuration

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
-use std::{ fs::{ File, create_dir_all, read_to_string }, path::Path };
-
-use dirs::config_dir;
+mod utils;
+use utils::config::init_config;
 
 fn main() {
-    let todos: String = get_todos();
-    println!("{}", get_todo_by_id(&todos, 3).unwrap_or(String::new()));
-}
-
-fn get_todos() -> String {
-    let folder_path: String = format!("{}/stodo", config_dir().unwrap().to_str().unwrap());
-    let file_path: String = format!("{}/todos.txt", &folder_path);
-
-    create_dir_all(&folder_path).expect("Could not create todo folder.");
-
-    if !Path::new(&file_path).is_file() {
-        File::create(&file_path).expect("Could not create todo file.");
-    }
-
-    return read_to_string(&file_path).expect("Could not read todo file.").trim().to_string();
-}
-
-fn get_todo_by_id(todos: &str, id: usize) -> Result<String, String> {
-    if todos.len() == 0 {
-        return Err(String::from("Todo list is empty."));
-    }
-
-    if todos.lines().nth(id).is_none() {
-        return Err(format!("Item at index {} is null", id));
-    }
-
-    return Ok(String::from(todos.lines().nth(id).unwrap_or("")));
+    init_config();
 }
---
+++ src/utils/config.rs
@@ -0,0 +1,37 @@
+use std::{ path::Path, fs::{ create_dir_all, File } };
+
+pub struct Configuration {
+    root_dir: String,
+    config_file: String,
+    todo_list_file: String,
+}
+
+pub fn init_config() -> Configuration {
+    let root_dir = format!("{}/stodo", dirs::config_dir().unwrap().to_str().unwrap());
+
+    let config = Configuration {
+        root_dir: format!("{}", root_dir),
+        config_file: format!("{}/config.toml", root_dir),
+        todo_list_file: format!("{}/todo_list.txt", root_dir),
+    };
+
+    if !Path::new(&config.root_dir).is_dir() {
+        create_dir_all(&config.root_dir).expect(
+            format!("Could not create root folder: {}", &config.root_dir).as_str()
+        );
+    }
+
+    if !Path::new(&config.config_file).is_file() {
+        File::create(&config.config_file).expect(
+            format!("Could not create config file: {}", &config.config_file).as_str()
+        );
+    }
+
+    if !Path::new(&config.todo_list_file).is_file() {
+        File::create(&config.todo_list_file).expect(
+            format!("Could not create todos file: {}", &config.config_file).as_str()
+        );
+    }
+
+    return config;
+}
